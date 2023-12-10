9ef82e4ad303e6033150ea8de06f4f455a25b563
Dayeeet <david@cappell.net>
2023-12-10 18:56:51 +0100

    feat: config support

---
+++ prefixes-shared/src/main/kotlin/app/simplecloud/plugin/prefixes/shared/PrefixesConfigParser.kt
@@ -0,0 +1,16 @@
+package app.simplecloud.plugin.prefixes.shared
+
+import app.simplecloud.plugin.prefixes.api.PrefixesConfig
+import com.google.gson.Gson
+import com.google.gson.stream.JsonReader
+import java.io.File
+import java.io.FileReader
+
+class PrefixesConfigParser<C : PrefixesConfig>(private val configFile: File) {
+    fun parse(type: Class<C>, default: C) : C {
+        if(!configFile.exists()) return default
+        val gson = Gson()
+        val reader = JsonReader(FileReader(configFile))
+        return gson.fromJson(reader, type)
+    }
+}
